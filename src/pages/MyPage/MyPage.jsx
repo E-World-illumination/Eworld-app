b198af7aafa48ef7cbd8dedb217c48629eb19aad
daabaat <lllllls189@gmail.com>
2024-12-04 14:27:16 +0900

    tailwind 추ㅏㄱ

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,7 +1,13 @@
 import React, { useState, useContext } from "react";
 
 const MyPage = () => {
-  return <>MyPage</>;
+  return (
+    <>
+      <div className="text-blue-300 text-xl font-bold">
+        tailwind 시험 mypage
+      </div>
+    </>
+  );
 };
 
 export default MyPage;
