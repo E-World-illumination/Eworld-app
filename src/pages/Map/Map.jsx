664649fcf792fe8c555372f2588e6d52cc77d7dc
textwr <wjdrb6940@gmail.com>
2024-12-04 15:29:01 +0900

    12-04

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -4,53 +4,26 @@ import { AuthContext } from "../../App";
 import styles from "./styles.module.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import { Map } from "react-kakao-maps-sdk";
 
-const { kakao } = window;
-
-const Map = () => {
+const kakaoMap = () => {
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    console.log(kakao);
-    let container = document.getElementById("map");
-    let options = {
-      center: new kakao.maps.LatLng(35.85336, 128.563916),
-      level: 3,
-    };
-
-    let map = new kakao.maps.Map(container, options);
-
-    // 마커
-    let markerPosition = new kakao.maps.LatLng(35.85336, 128.563916);
-    let marker = new kakao.maps.Marker({
-      position: markerPosition,
-    });
-    marker.setMap(map);
-
-    // 인포윈도우
-    let iwContent = '<div style="padding:5px;">테스트</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-      iwPosition = markerPosition; //인포윈도우 표시 위치입니다
-
-    var infowindow = new kakao.maps.InfoWindow({
-      position: iwPosition,
-      content: iwContent,
-    });
-    infowindow.open(map, marker);
-
-    // 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, "click", function () {
-      console.log("clicked");
-    });
-  }, []);
-
   return (
-    <div className={styles.mapPage}>
-      <Header title="Map"></Header>
-      <div id="map" style={{ width: "500px", height: "400px" }}></div>
-      <Footer></Footer>
-    </div>
+    <>
+      <div className={styles.mapPage}>
+        <Header title="Map"></Header>
+        <Map
+          className={styles.kakaoMap}
+          center={{ lat: 35.85336, lng: 128.563916 }}
+          style={{ width: "100%", flex: "1 0 auto" }}
+          level={3}
+        />
+        <Footer></Footer>
+      </div>
+    </>
   );
 };
 
-export default Map;
+export default kakaoMap;
