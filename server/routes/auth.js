5cb720be202dfed0757ad7873dcaffcce4636f67
textwr <wjdrb6940@gmail.com>
2024-12-12 17:40:39 +0900

    서버

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,15 @@
-import express from "express";
-import { db, getToken } from "../db.js";
+import express, { Router } from "express";
+import { db, getToken, getDate } from "../db.js";
 import dotenv from "dotenv";
 
-import { router as googleHandler } from "./handler/googleHandler.js";
-import { router as kakaoHandler } from "./handler/kakaoHandler.js";
-import { router as naverHandler } from "./handler/naverHandler.js";
+import { router as googleHandler } from "./auth/googleHandler.js";
+import { router as kakaoHandler } from "./auth/kakaoHandler.js";
+import { router as naverHandler } from "./auth/naverHandler.js";
 
 const router = express.Router();
 
 // 일반 회원가입
-router.post("/signin", async (req, res) => {
+router.post("/signup", async (req, res) => {
   const { id, password, name, email, phone } = req.body;
 
   if (!id || !password || !name) {
@@ -26,9 +26,11 @@ router.post("/signin", async (req, res) => {
     phone,
   ]);
 
-  if (duplication[0]) {
+  if (duplication[0].length > 0) {
     let text = "";
-    if (duplication[0].social != null) {
+    console.log(duplication[0][0].social);
+    console.log(duplication[0][0].social === null);
+    if (duplication[0][0].social != null) {
       text = `${duplication[0].social}을 통해 로그인하여 주시길 바랍니다.`;
     }
     return res
@@ -37,10 +39,12 @@ router.post("/signin", async (req, res) => {
   }
 
   // 회원가입 처리
+  // Malformed communication packet. error 1835 이슈 있어서 하드코딩함
   try {
     const [results, fields] = await db.execute(
-      "INSERT INTO `user`(`name`, `password`, `id`, `email`, `created_at`) VALUES (?,?,?,?,?)",
-      [name, password, id, email, phone, getDate()]
+      /*"INSERT INTO `user`(`name`, `password`, `id`, `email`, `created_at`) VALUES (?,?,?,?,?)",
+      [name, password, id, email, phone, getDate()]*/
+      `INSERT INTO user (name, id, password, phone, email, profile_img, social, created_at) VALUES ('${name}', '${id}', '${password}', '${phone}', '${email}', NULL, NULL, '${getDate()}')`
     );
     const jwtToken = getToken({ id: id, name: name, social: null });
     res.status(200).json({
@@ -49,10 +53,36 @@ router.post("/signin", async (req, res) => {
       data: { token: jwtToken },
     });
   } catch (e) {
+    console.log(e);
     res.status(500).json({
       status: "fail",
       message: "회원가입 실패",
       data: {},
+    });
+  }
+});
+
+// 아이디 중복 확인
+router.post("/duplicate", async (req, res) => {
+  const { id } = req.body;
+  if (!id) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "아이디를 입력해주세요." });
+  }
+  const duplication = await db.execute(
+    "SELECT * FROM user WHERE id = ? AND social IS NULL;",
+    [id]
+  );
+  if (duplication[0][0] === undefined) {
+    res.status(200).json({
+      status: "success",
+      message: "중복되는 아이디 없음.",
+    });
+  } else {
+    res.status(400).json({
+      status: "error",
+      message: "아이디 중복",
     });
   }
 });
@@ -78,8 +108,9 @@ router.post("/login", async (req, res) => {
   }
 
   const jwtToken = getToken({
-    id: result[0].id,
-    name: result[0].name,
+    key: result[0][0].key,
+    id: result[0][0].id,
+    name: result[0][0].name,
     social: null,
   });
   res.status(200).json({
---
+++ server/routes/user.js
@@ -0,0 +1,166 @@
+import express from "express";
+import { db, verifyToken } from "../db.js";
+import JWT from "jsonwebtoken";
+
+const router = express.Router();
+
+function sendError(status, res, message) {
+  return res.status(status).json({
+    status: "error",
+    message: message,
+  });
+}
+
+router.get("/stamp", async (req, res) => {
+  const token = req.get("Authorization");
+
+  const userData = await verifyToken(token);
+
+  if (userData === "error") {
+    return sendError(400, res, "잘못된 토큰입니다");
+  }
+
+  const result = await db.execute(
+    "SELECT stamp FROM user_stamp WHERE user = ?;",
+    [userData.key]
+  );
+  return res.status(200).json({
+    status: "success",
+    message: "데이터 전송 완료.",
+    data: result[0],
+  });
+});
+
+router.post("/modify", async (req, res) => {
+  const token = req.get("Authorization");
+  const { phone, email, currentPassword, changePassword } = req.body;
+
+  const userData = await verifyToken(token);
+
+  if (userData === "error") {
+    return sendError(400, res, "잘못된 토큰입니다");
+  }
+
+  let query = "UPDATE `user` SET ";
+  const arr = [];
+
+  if (phone != null) {
+    query += "`phone` = ?";
+    arr.push(phone);
+  }
+
+  if (email != null) {
+    if (arr.length > 0) {
+      query += ",";
+    }
+    query += "`email` = ?";
+    arr.push(email);
+  }
+
+  //비밀번호 변경 로직
+  if (currentPassword) {
+    if (!changePassword) {
+      return sendError(400, res, "변경할 비밀번호를 작성해 주세요.");
+    }
+
+    const result = await db.execute(
+      "SELECT `password` FROM `user` WHERE `key` = ?;",
+      [userData.key]
+    );
+
+    if (result[0][0].password != currentPassword) {
+      return sendError(400, res, "잘못된 기존 비밀번호를 입력하셨습니다.");
+    } else {
+      if (arr.length > 0) {
+        query += ",";
+      }
+      query += "`password` = ?";
+      arr.push(changePassword);
+    }
+  }
+
+  query += " WHERE `key` = ?;";
+  arr.push(userData.key);
+
+  console.log(query);
+  console.log(arr);
+
+  try {
+    const result = await db.execute(query, arr);
+    return res
+      .status(200)
+      .json({ status: "success", message: "변경에 성공하였습니다" });
+  } catch (e) {
+    console.log(e);
+    return sendError(500, res, "처리중 에러가 발생하였습니다.");
+  }
+});
+
+// 회원정보
+router.get("/data", async (req, res) => {
+  const token = req.get("Authorization");
+  const userData = await verifyToken(token);
+  if (userData === "error") {
+    return sendError(400, res, "잘못된 토큰입니다");
+  }
+
+  try {
+    const result = await db.execute(
+      "SELECT `name`, `profile_img`, `phone`, `email`, `social` FROM `user` WHERE `key` = ?;",
+      [userData.key]
+    );
+
+    return res.status(200).json({
+      status: "success",
+      message: "고객 정보를 불러오는데 성공했습니다.",
+      data: result[0][0],
+    });
+  } catch (e) {
+    console.log(e);
+    return sendError(500, res, "오류가 발생하였습니다.");
+  }
+});
+
+// 쿠폰 리스트
+router.get("/coupon", async (req, res) => {
+  const token = req.get("Authorization");
+  const userData = await verifyToken(token);
+  if (userData === "error") {
+    return sendError(400, res, "잘못된 토큰입니다");
+  }
+
+  try {
+    const result = await db.execute(
+      "SELECT `name`, `content`, `created_at`, `expired_at`, `is_used` FROM `user_coupon` WHERE `user` = ?;",
+      [userData.key]
+    );
+
+    return res.status(200).json({
+      status: "success",
+      message: "쿠폰 정보를 불러오는데 성공했습니다.",
+      data: result[0],
+    });
+  } catch (e) {
+    console.log(e);
+    return sendError(500, res, "오류가 발생하였습니다.");
+  }
+});
+
+router.get("/stamp_course", async (req, res) => {
+  try {
+    const result = await db.execute(
+      "SELECT `latitude`, `longitude`, `name`, `name_kr`, `description`, `qr` FROM `stamp_place`;"
+    );
+
+    return res.status(200).json({
+      status: "success",
+      message: "장소 정보를 불러오는데 성공했습니다.",
+      data: result[0],
+    });
+  } catch (e) {
+    console.log(e);
+    return sendError(500, res, "오류가 발생하였습니다.");
+  }
+});
+
+export { router };
