2127a33b5a7f6576b34bed7e49c8bd79d2d16c34
daabaat <lllllls189@gmail.com>
2024-12-17 09:45:12 +0900

    viteconfig 수정

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,6 @@ import "./index.css";
 
 import App from "./App.jsx";
 
-const loadKakaoMapScript = () => {
-  const script = document.createElement("script");
-  script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.VITE_KAKAOMAP_KEY}`;
-  script.async = true;
-  document.head.appendChild(script);
-};
-
-loadKakaoMapScript();
-
 createRoot(document.getElementById("root")).render(
   /*<StrictMode>
   </StrictMode>,*/
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: "html-transform",
+      transformIndexHtml(html) {
+        return html.replace(
+          "%VITE_KAKAOMAP_KEY%",
+          process.env.VITE_KAKAOMAP_KEY,
+        );
+      },
+    },
+  ],
+});
