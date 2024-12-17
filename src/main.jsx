5b8886fd6c970f8adc4a77bd00611cca8bbaf7a5
daabaat <lllllls189@gmail.com>
2024-12-17 09:27:04 +0900

    index.html수정

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,15 @@ import "./index.css";
 
 import App from "./App.jsx";
 
+const loadKakaoMapScript = () => {
+  const script = document.createElement("script");
+  script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_MAP_API_KEY}`;
+  script.async = true;
+  document.head.appendChild(script);
+};
+
+loadKakaoMapScript();
+
 createRoot(document.getElementById("root")).render(
   /*<StrictMode>
   </StrictMode>,*/
